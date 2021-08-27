e6c2a7395c0d214663a169031758677d7c98737c
jurikiin <kiin.juri@gmail.com>
2021-08-26 21:32:58 -0400

    Created template site

---
+++ local-includes.js
@@ -0,0 +1,26 @@
+class KSInclude extends HTMLElement {
+    constructor() {
+        super();
+    }
+    
+    static get observedAttributes() {
+        return ['data'];
+    }
+
+    connectedCallback() {
+        this.render();
+      }
+
+    render() {
+        fetch(this.getAttribute('data'))
+        .then(res => res.text())
+        .then(include => this.innerHTML = include)
+        .catch(err => console.error(err));
+    }
+
+    attributeChangedCallback() {
+        this.render();
+    }
+}
+
+customElements.define("ks-include", KSInclude);
