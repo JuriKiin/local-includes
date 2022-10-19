8324145bcd6ee205aa3a6dda727609c5d655c50c
Jüri Kiin <kiin.juri@gmail.com>
2022-10-19 19:01:32 -0400

    Revert Structure

--- local-includes.js
+++ local-includes.js
@@ -1,5 +1,49 @@
-import { KSInclude } from './js/ks-include';
-import { KSTemplate } from './js/ks-template';
+class KSInclude extends HTMLElement {
+    constructor() {
+        super();
+    }
+    
+    static get observedAttributes() {
+        return ['path'];
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        fetch(this.getAttribute('path'))
+            .then(res => res.text())
+            .then(include => this.innerHTML = include)
+            .catch(err => console.error(err));
+    }
+
+    attributeChangedCallback() {
+        this.render();
+    }
+}
+
+class KSTemplate extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    static get observedAttributes() {
+        return ['data', 'path'];
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        fetch(this.getAttribute('path'))
+            .then(res => res.text())
+            .then(template => this.innerHTML = template)
+            .catch(err => console.error(err));
+    }
+}
 
 customElements.define("ks-include", KSInclude);
-// customElements.define("ks-template", KSTemplate);
+//customElements.define("ks-template", KSTemplate);
+
