cb37b6c07c03176176e724dd841703b7079c6576
Jüri Kiin <kiin.juri@gmail.com>
2022-10-19 18:55:28 -0400

    Renamed data to path for include
    Restructured diles

--- local-includes.js
+++ local-includes.js
@@ -1,26 +1,5 @@
-class KSInclude extends HTMLElement {
-    constructor() {
-        super();
-    }
-    
-    static get observedAttributes() {
-        return ['data'];
-    }
-
-    connectedCallback() {
-        this.render();
-      }
-
-    render() {
-        fetch(this.getAttribute('data'))
-        .then(res => res.text())
-        .then(include => this.innerHTML = include)
-        .catch(err => console.error(err));
-    }
-
-    attributeChangedCallback() {
-        this.render();
-    }
-}
+import { KSInclude } from './js/ks-include';
+import { KSTemplate } from './js/ks-template';
 
 customElements.define("ks-include", KSInclude);
+// customElements.define("ks-template", KSTemplate);
